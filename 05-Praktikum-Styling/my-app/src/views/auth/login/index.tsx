5b0cc67244ff76ad144e91349729b339249407bd
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2026-02-27 09:43:28 +0700

    9. Tailwind CSS

--- 05-Praktikum-Styling/my-app/src/views/auth/login/index.tsx
+++ 05-Praktikum-Styling/my-app/src/views/auth/login/index.tsx
@@ -10,7 +10,7 @@ const TampilanLogin = () => {
     }
   return (
     <div className={styles.login}>
-        <h1>Halaman Login</h1>
+        <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
         <button onClick={() => handlerLogin()}>Login</button> <br />
         <h1 style={{ color: "red",borderRadius: "10px",padding: "10px",}}>Belum Punya Akun</h1>
         <Link href="/auth/register">Ke Halaman Register</Link>
