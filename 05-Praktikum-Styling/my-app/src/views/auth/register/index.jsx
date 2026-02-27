6ddca60d429eda4e3784b2c0b607568092bd9494
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2026-02-27 09:56:46 +0700

    TugasPraktikum1

--- 05-Praktikum-Styling/my-app/src/views/auth/register/index.jsx
+++ 05-Praktikum-Styling/my-app/src/views/auth/register/index.jsx
@@ -1,12 +1,39 @@
 import Link from "next/link";
+import styles from "./register.module.css";
 
-const halamanRegister = () => {
+const HalamanRegister = () => {
   return (
-    <div>
-        <h1>Halaman Register</h1>
-        <Link href="/auth/login">Ke Halaman Login</Link>
-    </div>
-    )
-}
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>Halaman Register</h1>
 
-export default halamanRegister;
+        <form>
+          <div className={styles.inputGroup}>
+            <label>Nama</label>
+            <input type="text" className={styles.input} />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label>Email</label>
+            <input type="email" className={styles.input} />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label>Password</label>
+            <input type="password" className={styles.input} />
+          </div>
+
+          <button type="submit" className={styles.button}>
+            Register
+          </button>
+        </form>
+
+        <Link href="/auth/login" className={styles.link}>
+          Sudah punya akun? Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HalamanRegister;
