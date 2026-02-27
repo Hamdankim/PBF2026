57e9ff837f87621bb5169f4812146811ea2c282a
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2026-02-27 09:16:58 +0700

    5. Refactoring Struktur Project (Best Practice)

---
+++ 05-Praktikum-Styling/my-app/src/views/auth/login/index.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+import styles from './login.module.css';
+
+const TampilanLogin = () => {
+    const {push} = useRouter();
+    const handlerLogin = () => {
+        push("/produk");
+    }
+  return (
+    <div className={styles.login}>
+        <h1>Halaman Login</h1>
+        <button onClick={() => handlerLogin()}>Login</button> <br />
+        <Link href="/auth/register">Ke Halaman Register</Link>
+    </div>
+  )
+}
+
+export default TampilanLogin;
