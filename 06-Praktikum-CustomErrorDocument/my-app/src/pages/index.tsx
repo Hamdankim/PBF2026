2b1e2a817bb22031928ce625dcd2df070d780757
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2026-02-28 14:18:45 +0700

    Langkah 3 – Pengaturan Title per Halaman

--- 06-Praktikum-CustomErrorDocument/my-app/src/pages/index.tsx
+++ 06-Praktikum-CustomErrorDocument/my-app/src/pages/index.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
+      <head>
+        <title>Praktikum Next.js Pages Router</title>
+      </head>
       <h1>Praktikum Next.js Pages Router</h1> <br />
       <p>Mahasiswa D4 Pengembangan Web</p> <br />
       <p><Link href="/about">About Page</Link></p>
