12fee20a8f70e5032fe5343c47cab172bed78b71
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2026-03-02 18:11:33 +0700

    Bagian 5 – Implementasi SWR

--- 08-Praktikum-ClientSideRendering/my-app/src/pages/produk/index.tsx
+++ 08-Praktikum-ClientSideRendering/my-app/src/pages/produk/index.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 import TampilanProduk from "../../views/produk";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
 
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const Kategori = () => {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    fetch("/api/produk")
-      .then((response) => response.json())
-      .then((responsedata) => {
-        // Mengasumsikan struktur API adalah { data: [...] }
-        setProducts(responsedata.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching produk:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/produk")
+  //     .then((response) => response.json())
+  //     .then((responsedata) => {
+  //       // Mengasumsikan struktur API adalah { data: [...] }
+  //       setProducts(responsedata.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching produk:", error);
+  //     });
+  // }, []);
+
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data.data || []} />
     </div>
   );
 };
