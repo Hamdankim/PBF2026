acb24583a1b9e34d716a8726aff606602997c554
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2026-04-07 16:20:26 +0700

    Praktikum Jobsheet 19

--- 19-Praktikum-UnitTesting/my-app/src/views/product/index.tsx
+++ 19-Praktikum-UnitTesting/my-app/src/views/product/index.tsx
@@ -13,45 +13,47 @@ type ProductType = {
 const TampilanProduk = ({ products }: { products: ProductType[] }) => {
   return (
     <div className={styles.produk}>
-      <h1 className={styles.produk__title}>Daftar Produk</h1>
+      <h1 className={styles.produk__title} data-testid="title">
+        Daftar Produk
+      </h1>
 
       <div className={styles.produk__content}>
         {products?.length > 0
           ? products.map((product: ProductType) => (
-            <Link
-              href={`/produk/${product.id}`}
-              key={product.id}
-              className={styles.produk__content__item}
-            >
-              <Image
-                src={product.image}
-                alt={product.name}
-                width={200}
-                height={200}
-                className={styles.produk__content__item__image}
-              />
+              <Link
+                href={`/produk/${product.id}`}
+                key={product.id}
+                className={styles.produk__content__item}
+              >
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  width={200}
+                  height={200}
+                  className={styles.produk__content__item__image}
+                />
 
-              <h4 className={styles.produk__content__item__name}>
-                {product.name}
-              </h4>
+                <h4 className={styles.produk__content__item__name}>
+                  {product.name}
+                </h4>
 
-              <p className={styles.produk__content__item__category}>
-                {product.category}
-              </p>
+                <p className={styles.produk__content__item__category}>
+                  {product.category}
+                </p>
 
-              <p className={styles.produk__content__item__price}>
-                Rp {product.price.toLocaleString("id-ID")}
-              </p>
-            </Link>
-          ))
+                <p className={styles.produk__content__item__price}>
+                  Rp {product.price.toLocaleString("id-ID")}
+                </p>
+              </Link>
+            ))
           : Array.from({ length: 4 }).map((_, index) => (
-            <div key={index} className={styles.produk__content__skeleton}>
-              <div className={styles.skeleton__image}></div>
-              <div className={styles.skeleton__name}></div>
-              <div className={styles.skeleton__category}></div>
-              <div className={styles.skeleton__price}></div>
-            </div>
-          ))}
+              <div key={index} className={styles.produk__content__skeleton}>
+                <div className={styles.skeleton__image}></div>
+                <div className={styles.skeleton__name}></div>
+                <div className={styles.skeleton__category}></div>
+                <div className={styles.skeleton__price}></div>
+              </div>
+            ))}
       </div>
     </div>
   );
